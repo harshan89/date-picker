ac746047b358ad3fbb7d9a34e0a5495ce3277723
Harshan Morawaka <harshanweb@gmail.com>
2023-08-20 07:48:39 +0530

    date colors bound to variables

---
+++ src/utils/colorTheme.ts
@@ -0,0 +1,25 @@
+export const lightTheme = {
+    backgroundColor: "#fff",
+    wrapperBackgroundColor: "",
+    monthDateTextColor: "",
+    nonMonthDateTextColor: "",
+    weekNameTextColor: "",
+    arrowsColor: "",
+    arrowsHoverColor: "",
+    dateHoverBorder: "",
+    dateHoverBackgroundColor: "",
+    dateSelectedBackgroundColor: ""
+}
+
+export const darkTheme = {
+    backgroundColor: "#1A1A1A",
+    wrapperBackgroundColor: "#595959",
+    monthDateTextColor: "#fff",
+    nonMonthDateTextColor: "#595959",
+    weekNameTextColor: "#fff",
+    arrowsColor: "#fff",
+    arrowsHoverColor: "#F7CC45",
+    dateHoverBorder: "1px solid #fff",
+    dateHoverBackgroundColor: "#1A1A1A",
+    dateSelectedBackgroundColor: "#F7CC45"
+}
